723a1b1eb1c20cac27acfd04e4b12ad067b8e67e
YadinaM <yadina.moreira@gmail.com>
2023-10-27 12:14:13 +0200

    feat: add .env

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+require("dotenv").config();
+console.log(process.env.MONGODB);
 
 // connect to mongodb
 mongoose.connect("mongodb://127.0.0.1:27017/messagesApi");
